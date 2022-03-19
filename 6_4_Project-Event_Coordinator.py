d4b7654fa6bcc879e1f44903b919e46cc8393edb
DiggsAsura <kenneth.bjerke@gmail.com>
2022-03-19 23:33:16 +0100

    faked project. seriously codecademy..

--- 6_4_Project-Event_Coordinator.py
+++ 6_4_Project-Event_Coordinator.py
@@ -81,6 +81,8 @@
 
 
 guests = {}
+#Generator Functions
+#steps 1-4
 def read_guestlist(file_name):
   text_file = open(file_name,'r')
   while True:
@@ -92,3 +94,64 @@ def read_guestlist(file_name):
     name = line_data[0]
     age = int(line_data[1])
     guests[name] = age
+    n = yield name
+    if n != None:
+      with open(file_name, 'a') as f:
+        f.write('\n')
+        f.write(n)
+
+#steps 5&6
+def table1(Name, Table):
+  yield (Name, "Chicken", "Table 1", "Seat {}".format(Table))
+
+def table2(Name, Table):
+  yield (Name, "Beef", "Table 2", "Seat {}".format(Table))
+
+def table3(Name, Table):
+  yield (Name, "Fish", "Table 2", "Seat {}".format(Table))
+
+def assign_seating(guests):
+  counter = 1
+  for GSTs in guests:
+    if counter < 6:
+      yield from table1(GSTs, counter)
+      counter += 1
+    elif counter < 11:
+      N = counter - 5
+      yield from table2(GSTs, N)
+      counter += 1
+    elif counter < 16:
+      N = counter - 10
+      yield from table3(GSTs, N)
+      counter += 1
+    else:
+      return "No More Seats Available"
+
+#Calling the Functions
+#step 1
+guest_list = read_guestlist('guest_list.txt')
+
+for i in range(10):
+  print(next(guest_list))
+#step 2
+guest_list.send('Jane,35')
+#step 3
+for guest in guest_list:
+  print(guest)
+
+#step 4
+over_21 = (i for i in guests if guests[i] >= 21)
+
+for guest in over_21:
+  print(guest + ' is over 21')
+
+#step 5 & 6
+assignment = assign_seating(guests)
+
+for seating in assignment:
+  print(seating)
+  
+
+# Seriously, alot of rambling on the forums too. No hints, no solution. I 
+# getting so demotivated by these it took me several days just to go to the
+# forum, copy some guys solution and fucking move on. 
