d5b445333f28361bb5875bda5fe585b9ac4364a6
DiggsAsura <kenneth.bjerke@gmail.com>
2022-04-11 10:45:24 +0200

    8_2 Project: Aisha's Greeting

---
+++ 8_2_Project__Aishas_Greetings.py
@@ -0,0 +1,22 @@
+# 8. Resource Management
+# 2. Project
+# Aisha's Greetings
+
+print("\nAisha's Greetings\n")
+
+# You are a part of Aisha's Greetings - a card printing company that prints greetings
+# cards with a hint of personalization! You want to help Aisha create a system that
+# generates cards based on customer's requests.
+#
+# You have been provided with two pre-filled card types:
+#
+# - happy_bday.txt: a card with a birthday message
+#
+# - thankyou_card.txt: a card file with a thank you message
+#
+# Click through the files on your right and see what's in them! If you get stuck,
+# you can look at a sample solution of the project by looking at greetings_solutions.py
+# - but try it out on your own first.
+#
+# Let's start printing!
+
